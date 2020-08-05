97898d053819756bd434b3d56dbf53f8b0bc4888
Erman Kemanci <ekemanci01@hotmail.com>
2020-08-05 18:26:39 +0100

    Initial Setup And Created Fake Data by Bogus

---
+++ UserManagement/UserManagement.API/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagement.API.Fake;
+using UserManagement.API.Models;
+
+namespace UserManagement.API.Controllers
+{
+    [Route("api/[Controller]")]
+    public class UsersController:ControllerBase
+    {
+        private List<User> _users = FakeData.GetUsers(200);
+
+        public List<User> Get()
+        {
+            return _users;
+        }
+    }
+}
---
+++ UserManagement/UserManagement.API/Fake/FakeData.cs
@@ -0,0 +1,28 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagement.API.Models;
+
+namespace UserManagement.API.Fake
+{
+    public static class FakeData
+    {
+        private static List<User> _users;
+        public static List<User> GetUsers(int number)
+        {
+            if (_users == null)
+            {
+                _users = new Faker<User>()
+                               .RuleFor(u => u.Id, f => f.IndexFaker + 1)
+                               .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+                               .RuleFor(u => u.LastName, f => f.Name.LastName())
+                               .RuleFor(u => u.Address, f => f.Address.FullAddress())
+                               .Generate(number);
+            }
+
+            return _users;
+        }
+    }
+}
