8e301a67c4df830e77f31cbd66d37fdaad7c793b
Erman Kemanci <ekemanci01@hotmail.com>
2020-08-05 19:17:49 +0100

    Successfully Completed CRUD Operations Only on Runtime By Using Postman

--- UserManagement/UserManagement.API/Controllers/UsersController.cs
+++ UserManagement/UserManagement.API/Controllers/UsersController.cs
@@ -13,9 +13,39 @@ namespace UserManagement.API.Controllers
     {
         private List<User> _users = FakeData.GetUsers(200);
 
+        [HttpGet]
         public List<User> Get()
         {
             return _users;
+        }
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            var user = _users.FirstOrDefault(u => u.Id == id);
+
+            return user;
+        }
+        [HttpPost]
+        public User Post([FromBody]User user)
+        {
+            _users.Add(user);
+            return user;
+        }
+        [HttpPut]
+        public User Put([FromBody]User user)
+        {
+            var userToUpdate = _users.FirstOrDefault(u => u.Id == user.Id);
+            userToUpdate.FirstName = user.FirstName;
+            userToUpdate.LastName = user.LastName;
+            userToUpdate.Address = user.Address;
+
+            return user;
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var userToDelete = _users.FirstOrDefault(u => u.Id == id);
+            _users.Remove(userToDelete);
         }
     }
 }
